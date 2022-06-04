0139fbf1303743cb7a41453934646487b4ccba91
algovitae <algovitae@zohomail.com>
2022-06-04 00:51:28 +0000

    login mutation

---
+++ src/api/graphql/Login.graphql.ts
@@ -0,0 +1,16 @@
+import { gql } from "@apollo/client";
+
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const LoginMutation = gql`
+mutation Login($email: String, $password: String) {
+    login(email: $email, password: $password) {
+      token,
+      user_identity {
+        email, 
+        user {
+          user_name
+        }
+      }
+    }
+  }`
--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,17 +1,27 @@
 import { Button, Card, Skeleton, Table } from "antd"
 import React from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilCallback, useRecoilValue } from "recoil";
+import { apolloClient } from "../../api";
+import { LoginDocument, LoginMutation } from "../../api/generated";
 import { allUsersSelector } from "../../selectors/user";
 
 const TopPageContent = () => {
     const data = useRecoilValue(allUsersSelector);
-    console.log(data)
-    return <Table dataSource={data} columns={[{dataIndex: "user_name"}]} />
+    return <Table dataSource={data} columns={[{ dataIndex: "user_name" }]} rowKey="user_id" />
 }
 
 const TopPage: React.FC = () => {
+    const login = useRecoilCallback(() => async () => {
+        const { data, errors } = await apolloClient.mutate<LoginMutation>({ mutation: LoginDocument, variables: { email: 'hoge@example.com', password: 'password' } })
+        if (errors) {
+            console.error(errors)
+            return;
+        }
+        console.log(data?.login);
+        return;
+    })
     return <Card>
-        <Button>button</Button>
+        <Button onClick={login}>button</Button>
         <React.Suspense fallback={<Skeleton />}>
             <TopPageContent />
         </React.Suspense>
